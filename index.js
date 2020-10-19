1d6aeea659f79639456e0f60c329553f1b2c2fb0
James Donovan <jameswilliamdonovan@gmail.com>
2020-10-19 02:14:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function breakOut(array, changeValue, stopValue) {
   array.forEach(changeValue);
 }
+
+function keepGoing(array, changeValue, skipValue) {
+  array.forEach();
+}
+
+function findBy(array, findFn) {
+  
+}
